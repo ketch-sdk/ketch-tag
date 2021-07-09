36d72e986909ab4f858457808d8040c81dc75d32
smalexan <sam@ketch.com>
2021-07-09 15:34:24 -0700

    fix(build): tsc

--- src/internal/logging.ts
+++ src/internal/logging.ts
@@ -1,14 +1,19 @@
-import * as loglevel from 'loglevel';
+import loglevel, {LogLevelDesc} from 'loglevel';
 import parameters from './parameters';
 
 // Setup the log level based on a query string parameter.
 if (parameters.get(parameters.DEBUG, window.location.search)) {
   loglevel.setLevel('debug', true);
 } else {
-  const ll = parameters.get(parameters.LOG_LEVEL, window.location.search) as loglevel.LogLevelDesc;
+  const ll = parameters.get(parameters.LOG_LEVEL, window.location.search) as LogLevelDesc;
   if (ll) {
     loglevel.setLevel(ll, true);
   }
 }
+
+const originalFactory = loglevel.methodFactory;
+loglevel.methodFactory = (methodName, logLevel, loggerName) => (...message): void => {
+  originalFactory(methodName, logLevel, loggerName)(`[semaphore] ${String(loggerName)}`, ...message);
+};
 
 export default loglevel;
