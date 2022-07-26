7ea70a1ee869a6b61c55b6e50b66a5a9aaef289d
Sam Alexander <35970568+smalexan@users.noreply.github.com>
2022-07-25 22:54:20 -0700

    fix(shoreline): url (#194)

--- src/pure.ts
+++ src/pure.ts
@@ -53,10 +53,14 @@ enum ExperienceHidden {
 function getApiUrl(config: ketchapi.Configuration): string {
   if (config.services) {
     let url = config.services[constants.SHORELINE]
-    if (!url.endsWith('/')) {
-      url = url + '/';
+    // url must not end in '/'
+    if (url.endsWith('/')) {
+      url = url.slice(0, -1)
     }
-    url = url + 'web/v2';
+    // url must end in /web/v2 if not set
+    if (!url.endsWith('/web/v2')) {
+      url = url + '/web/v2';
+    }
     return url
   }
 
