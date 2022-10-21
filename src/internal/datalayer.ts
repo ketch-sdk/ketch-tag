59501aee2fb65455af58f5f1917727f5d23cd533
Naveed Akhtar <naveed.akhtar@ketch.com>
2022-10-21 14:27:53 +0530

    feat(KD-4486): ketch permit change tealium datalayer (#247)

--- src/internal/datalayer.ts
+++ src/internal/datalayer.ts
@@ -1,9 +1,24 @@
 declare global {
   interface Window {
     dataLayer: any[];
+    utag_data: TealiumketchPermitData;
+  }
+
+  type TealiumketchPermitData = {
+    [key: string]: any;
+    ketchPermit?: any;
   }
 }
 
 export default function datalayer(): any[] {
   return (window.dataLayer = window.dataLayer || [])
+}
+
+export function tealiumKetchPermitData(): any {
+  window.utag_data = tealiumDataLayer()
+  return (window.utag_data.ketchPermit = window.utag_data?.ketchPermit || {})
+}
+
+export function tealiumDataLayer(): any {
+  return (window.utag_data = window.utag_data || {})
 }
