176a20cd0d70a22c1dfbb8fe02c27cb2cc8f2546
Seth Yates <29878+sethyates@users.noreply.github.com>
2023-01-03 11:31:30 -0800

    feat: extract gtm, aep and tealium

--- src/datalayer.ts
+++ src/datalayer.ts
@@ -1,19 +1,9 @@
 declare global {
   interface Window {
     dataLayer?: any[]
-    ketchPermitPreferences: any
-    adobeDataLayer: any[]
   }
 }
 
 export default function datalayer(): any[] {
   return (window.dataLayer = window.dataLayer || [])
-}
-
-export function ketchPermitPreferences(): any {
-  return (window.ketchPermitPreferences = window.ketchPermitPreferences || {})
-}
-
-export function adobeDataLayer(): any[] {
-  return (window.adobeDataLayer = window.adobeDataLayer || [])
 }
