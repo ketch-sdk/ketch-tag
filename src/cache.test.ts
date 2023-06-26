8a3e423e9b72e6d9954b683fbb4a3caf765cea2b
Sam Alexander <sam@ketch.com>
2023-06-25 20:25:54 -0700

    fix(cache): client consent

--- src/cache.test.ts
+++ src/cache.test.ts
@@ -1,7 +1,8 @@
 import { GetConsentRequest, GetConsentResponse, SetConsentRequest } from '@ketch-sdk/ketch-types'
-import { CACHED_CONSENT_KEY, getCachedConsent, setCachedConsent } from './cache'
+import {CACHED_CONSENT_KEY, getCachedConsent, PUBLIC_CONSENT_KEY_V1, setCachedConsent, setPublicConsent} from './cache'
 import { getDefaultCacher } from '@ketch-com/ketch-cache'
 import constants from './constants'
+import {getCookie} from "@ketch-sdk/ketch-data-layer";
 
 describe('cache', () => {
   const request = {
@@ -15,6 +16,63 @@ describe('cache', () => {
     propertyCode: 'axonic',
     purposes: {},
   } as GetConsentRequest
+
+  const getResponseWithConsent = {
+    collectedAt: 0,
+    environmentCode: constants.PRODUCTION,
+    identities: {
+      account_id: '123',
+    },
+    jurisdictionCode: 'gdpr',
+    organizationCode: 'axonic',
+    propertyCode: 'axonic',
+    purposes: {
+      foo: 'false',
+      bar: 'true',
+    },
+  } as GetConsentResponse
+
+  const getRequestWithConsent = {
+    collectedAt: 0,
+    environmentCode: constants.PRODUCTION,
+    identities: {
+      account_id: '123',
+    },
+    jurisdictionCode: 'gdpr',
+    organizationCode: 'axonic',
+    propertyCode: 'axonic',
+    purposes: {
+      foo: {
+        allowed: "true",
+        legalBasisCode: "consent_opt_in"
+      },
+      bar: {
+        allowed: "false",
+        legalBasisCode: "consent_opt_in"
+      }
+    },
+  } as GetConsentRequest
+
+  const setRequestWithConsent = {
+    collectedAt: 0,
+    environmentCode: constants.PRODUCTION,
+    identities: {
+      account_id: '123',
+    },
+    jurisdictionCode: 'gdpr',
+    organizationCode: 'axonic',
+    propertyCode: 'axonic',
+    purposes: {
+      foo: {
+        allowed: "true",
+        legalBasisCode: "consent_opt_in"
+      },
+      bar: {
+        allowed: "false",
+        legalBasisCode: "consent_opt_in"
+      }
+    },
+  } as SetConsentRequest
 
   const cacher = getDefaultCacher<SetConsentRequest | GetConsentRequest | GetConsentResponse>()
 
@@ -41,5 +99,45 @@ describe('cache', () => {
   it('returns cached response when set', async () => {
     await setCachedConsent(request as SetConsentRequest)
     expect(await getCachedConsent(request)).toEqual(request)
+  })
+
+  it('public cookie not set if no consent', async () => {
+    await setPublicConsent(request as GetConsentRequest)
+
+    expect(getCookie(window, PUBLIC_CONSENT_KEY_V1)).toBeFalsy()
+    expect(localStorage.getItem(PUBLIC_CONSENT_KEY_V1)).toBeFalsy()
+  })
+
+  it('public cookie set from GetConsentResponse', async () => {
+    await setPublicConsent(getResponseWithConsent as GetConsentResponse)
+
+    const publicCookie = getCookie(window, PUBLIC_CONSENT_KEY_V1)
+    const publicLocalStorage: string = localStorage.getItem(PUBLIC_CONSENT_KEY_V1) || ''
+    expect(JSON.parse(Buffer.from(publicCookie, 'base64').toString()))
+      .toEqual({'foo': 'denied', 'bar': 'granted'})
+    expect(JSON.parse(Buffer.from(publicLocalStorage, 'base64').toString()))
+      .toEqual({'foo': 'denied', 'bar': 'granted'})
+  })
+
+  it('public cookie set from GetConsentRequest', async () => {
+    await setPublicConsent(getRequestWithConsent as GetConsentRequest)
+
+    const publicCookie = getCookie(window, PUBLIC_CONSENT_KEY_V1)
+    const publicLocalStorage: string = localStorage.getItem(PUBLIC_CONSENT_KEY_V1) || ''
+    expect(JSON.parse(Buffer.from(publicCookie, 'base64').toString()))
+      .toEqual({'foo': 'granted', 'bar': 'denied'})
+    expect(JSON.parse(Buffer.from(publicLocalStorage, 'base64').toString()))
+      .toEqual({'foo': 'granted', 'bar': 'denied'})
+  })
+
+  it( 'public cookie set from SetConsentRequest', async () => {
+    await setPublicConsent(setRequestWithConsent as SetConsentRequest)
+
+    const publicCookie = getCookie(window, PUBLIC_CONSENT_KEY_V1)
+    const publicLocalStorage: string = localStorage.getItem(PUBLIC_CONSENT_KEY_V1) || ''
+    expect(JSON.parse(Buffer.from(publicCookie, 'base64').toString()))
+      .toEqual({'foo': 'granted', 'bar': 'denied'})
+    expect(JSON.parse(Buffer.from(publicLocalStorage, 'base64').toString()))
+      .toEqual({'foo': 'granted', 'bar': 'denied'})
   })
 })
--- src/cache.ts
+++ src/cache.ts
@@ -1,7 +1,9 @@
-import { GetConsentRequest, GetConsentResponse, SetConsentRequest } from '@ketch-sdk/ketch-types'
+import {GetConsentRequest, GetConsentResponse, SetConsentRequest} from '@ketch-sdk/ketch-types'
 import { getDefaultCacher } from '@ketch-com/ketch-cache'
+import {setCookie} from "@ketch-sdk/ketch-data-layer";
 
 export const CACHED_CONSENT_KEY = '_swb_consent_'
+export const PUBLIC_CONSENT_KEY_V1 = '_ketch_consent_v1_'
 export const CACHED_CONSENT_TTL = 300 // 5 min in s
 
 const consentCacher = getDefaultCacher<SetConsentRequest | GetConsentRequest | GetConsentResponse>()
@@ -44,4 +46,32 @@ export async function setCachedConsent(
   input.collectedAt = Math.floor(Date.now() / 1000)
 
   await consentCacher.setItem(CACHED_CONSENT_KEY, input)
+}
+
+export async function setPublicConsent(
+  input: SetConsentRequest | GetConsentRequest | GetConsentResponse,
+): Promise<void> {
+  if (Object.keys(input).length === 0) {
+    return
+  }
+
+  // create public consent object
+  const consent: {[key: string]: string} = {};
+  for (const key in input.purposes) {
+    const value = input.purposes[key];
+    if (typeof value === "string") {
+      consent[key] = value === "true"? "granted": "denied"
+    } else {
+      if (value.allowed) {
+        consent[key] = value.allowed === "true"? "granted": "denied"
+      }
+    }
+  }
+
+  // set public cookie and localStorage if consent
+  if ( Object.keys(consent).length > 0 ) {
+    const consentString = Buffer.from(JSON.stringify(consent)).toString("base64")
+    localStorage.setItem(PUBLIC_CONSENT_KEY_V1, consentString)
+    setCookie(window, PUBLIC_CONSENT_KEY_V1, consentString, CACHED_CONSENT_TTL)
+  }
 }
