57c216444a39020fd2da05c515cb61927b456c4b
Sam Alexander <35970568+smalexan@users.noreply.github.com>
2023-06-26 20:16:20 -0700

    fix(cache): client consent (#430)

--- src/cache.ts
+++ src/cache.ts
@@ -1,7 +1,9 @@
-import { GetConsentRequest, GetConsentResponse, SetConsentRequest } from '@ketch-sdk/ketch-types'
+import { Configuration, GetConsentRequest, GetConsentResponse, SetConsentRequest } from '@ketch-sdk/ketch-types'
 import { getDefaultCacher } from '@ketch-com/ketch-cache'
+import { setCookie } from '@ketch-sdk/ketch-data-layer'
 
 export const CACHED_CONSENT_KEY = '_swb_consent_'
+export const PUBLIC_CONSENT_KEY_V1 = '_ketch_consent_v1_'
 export const CACHED_CONSENT_TTL = 300 // 5 min in s
 
 const consentCacher = getDefaultCacher<SetConsentRequest | GetConsentRequest | GetConsentResponse>()
@@ -44,4 +46,52 @@ export async function setCachedConsent(
   input.collectedAt = Math.floor(Date.now() / 1000)
 
   await consentCacher.setItem(CACHED_CONSENT_KEY, input)
+}
+
+export async function setPublicConsent(
+  input: SetConsentRequest | GetConsentRequest | GetConsentResponse,
+  config: Configuration,
+): Promise<void> {
+  if (Object.keys(input).length === 0) {
+    return
+  }
+
+  // create public consent object
+  const consent: { [key: string]: { status: string; ketchPurposes?: string[] } } = {}
+  if (!config.purposes) {
+    return
+  }
+
+  for (const purpose of config.purposes) {
+    if (!Object.prototype.hasOwnProperty.call(input.purposes, purpose.code)) {
+      continue
+    }
+
+    const value = input.purposes[purpose.code]
+    let status = ''
+    if (typeof value === 'string') {
+      status = value === 'true' ? 'granted' : 'denied'
+    } else {
+      if (value.allowed) {
+        status = value.allowed === 'true' ? 'granted' : 'denied'
+      }
+    }
+
+    if (status.length == 0) {
+      continue
+    }
+    consent[purpose.code] = { status: status }
+
+    // TODO canonicalPurposeCode will be deprecated with the addition of ketchPurposeCodes
+    if (purpose.canonicalPurposeCode && purpose.canonicalPurposeCode.length > 0) {
+      consent[purpose.code].ketchPurposes = [purpose.canonicalPurposeCode]
+    }
+  }
+
+  // set public cookie and localStorage if consent
+  if (Object.keys(consent).length > 0) {
+    const consentString = Buffer.from(JSON.stringify(consent)).toString('base64')
+    localStorage.setItem(PUBLIC_CONSENT_KEY_V1, consentString)
+    setCookie(window, PUBLIC_CONSENT_KEY_V1, consentString, CACHED_CONSENT_TTL)
+  }
 }
