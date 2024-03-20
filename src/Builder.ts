d74b5a93d44c312dd7bf44194d8f7b96b1fc6592
Sam Alexander <35970568+smalexan@users.noreply.github.com>
2024-03-20 08:06:20 -0700

    fix(config): version param (#478)

--- src/Builder.ts
+++ src/Builder.ts
@@ -124,6 +124,14 @@ export default class Builder {
 
     const cfg = await this._api.getFullConfiguration(request)
 
+    // TODO remove post experience cut over
+    // This is uses the requested version of shoreline for experience cut over only if user experienceVersion param set
+    // getApiUrl has logic to replace the url
+    // This replaces the shoreline root url in the full config for use within the tag
+    if (experienceVersion && cfg.services) {
+      cfg.services[constants.API_SERVER] = getApiUrl(this._config)
+    }
+
     const k = new Ketch(this._api, cfg)
 
     await k.setEnvironment(env)
